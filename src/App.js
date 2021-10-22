5e5c788b9e988c43f78a18113ab1f0f977827a70
Radutiu Horatiu <61684220+Radutiu-Horatiu@users.noreply.github.com>
2021-10-22 15:13:29 +0300

    added redux and chakra ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import { doc, setDoc } from "@firebase/firestore";
 import { db } from "./firebase";
 import axios from "axios";
+import HomeScreen from "./screens/HomeScreen/HomeScreen";
+import { Route, Switch } from "react-router";
 
 function App() {
   React.useEffect(() => {
@@ -25,22 +26,11 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path="/">
+        <HomeScreen />
+      </Route>
+    </Switch>
   );
 }
 
---
+++ src/screens/HomeScreen/HomeScreen.jsx
@@ -0,0 +1,11 @@
+import { Flex } from '@chakra-ui/react'
+import { Text } from '@chakra-ui/layout'
+import React from 'react'
+
+export default function HomeScreen() {
+  return (
+    <Flex>
+      <Text>Saloot</Text>
+    </Flex>
+  )
+}
