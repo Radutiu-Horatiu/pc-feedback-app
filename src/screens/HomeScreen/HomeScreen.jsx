be30007b7a9fafbd0565e3e3443fa2701383a3db
Mihai Oancea <mihai.oancea@gutenbergcerts.com>
2021-11-05 15:15:39 +0200

    login + register + logout

--- src/screens/HomeScreen/HomeScreen.jsx
+++ src/screens/HomeScreen/HomeScreen.jsx
@@ -1,18 +1,44 @@
-import { Flex } from '@chakra-ui/react'
-import { Text } from '@chakra-ui/layout'
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { userActions } from '../../store/user/user-slice';
-export default function HomeScreen() {
-  const dispatch = useDispatch();
-	const username = useSelector(state => state.user.username);
+import { Flex } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/layout";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { userActions } from "../../store/user/user-slice";
+import { auth } from "../../firebase";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "@firebase/auth";
 
+export default function HomeScreen() {
+	const dispatch = useDispatch();
+	const username = useSelector((state) => state.user.username);
+	const email = useSelector((state) => state.user.email);
 	useEffect(() => {
-		dispatch(userActions.setUsername({username: "Dummy rname"}));
+		dispatch(userActions.setUsername({ username: "Dummy rname" }));
 	}, [dispatch, username]);
-  return (
-    <Flex>
-      <Text>{username}</Text>
-    </Flex>
-  )
+	useEffect(() => {
+		login("omihai00@gmail.com", "123456");
+	}, []);
+
+	const login = (email, pass) => {
+		signInWithEmailAndPassword(auth, email, pass)
+			.then((userCredentials) => {
+				dispatch(userActions.setEmail({ email: userCredentials.user.email }));
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+	};
+	const register = (email, pass) => {
+		createUserWithEmailAndPassword(auth, email, pass)
+			.then((userCredentials) => {
+				dispatch(userActions.setEmail({ email: userCredentials.user.email }));
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+	};
+	return (
+		<Flex>
+			<Text>{email}</Text>
+			<Text>{username}</Text>
+		</Flex>
+	);
 }
--- src/store/user/user-slice.js
+++ src/store/user/user-slice.js
@@ -1,18 +1,33 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+import { login, logout, register } from "./utils";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "@firebase/auth";
+import { auth } from "../../firebase";
 
 const initialState = {
-    username: "",
-}
+	username: "",
+	email: "",
+};
 
 const userSlice = createSlice({
-    name: 'user',
-    initialState,
-    reducers: {
-        setUsername(state, action)
-        {
-            state.username = action.payload.username;
-        }
-    }
+	name: "user",
+	initialState,
+	reducers: {
+		setUsername(state, action) {
+			state.username = action.payload.username;
+		},
+		setEmail(state, action) {
+			state.email = action.payload.email;
+		},
+		login(state, action) {
+			state.email = login(action.payload.email, action.payload.pass);
+		},
+		register(state, action) {
+			state.email = register(action.payload.email, action.payload.pass);
+		},
+		signOut(state) {
+			state.email = logout();
+		},
+	},
 });
 export const userActions = userSlice.actions;
 const userReducer = userSlice.reducer;
