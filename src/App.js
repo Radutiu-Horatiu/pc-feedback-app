75cb9328ea3b07938e135cf75628ec8f403a6a15
Radutiu Horatiu <61684220+Radutiu-Horatiu@users.noreply.github.com>
2021-11-19 14:52:51 +0200

    login/register pages working (#6)
    Co-authored-by: Mihai Oancea <50120497+mihib2000@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -8,60 +8,65 @@ import Navbar from "./screens/HomeScreen/Navbar";
 import { Flex, Text } from "@chakra-ui/layout";
 import { Button } from "@chakra-ui/button";
 import { FaPlus } from "react-icons/fa";
-import { useSelector } from "react-redux";
+import { onAuthStateChanged } from "@firebase/auth";
+import { auth } from "./firebase";
+import { useDispatch } from "react-redux";
+import { userActions } from "./store/user/user-slice";
+
 function App() {
-  const user = useSelector((state) => state.user);
-  const [loggedIn, setloggedIn] = useState(false);
+	const dispatch = useDispatch();
 
-  React.useEffect(() => {
-    if (!user) return;
-    setloggedIn(true);
-    // fastapi example
-    // (async () => {
-    //   const response = await axios.request({
-    //     method: "GET",
-    //     url: "http://127.0.0.1:8000/ceva",
-    //   });
-    //   console.log(response.data);
-    // })();
-  }, [user]);
+	React.useEffect(() => {
+		// fastapi example
+		// (async () => {
+		//   const response = await axios.request({
+		//     method: "GET",
+		//     url: "http://127.0.0.1:8000/ceva",
+		//   });
+		//   console.log(response.data);
+		// })();
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				console.log(user);
+				dispatch(userActions.setEmail({ email: user.email }));
+			}
+		});
+	}, []);
 
-  return (
-    <Flex h="100vh">
-      {loggedIn && <Navbar />}
+	return (
+		<Flex h="100vh">
+			<Navbar />
+			{/* Right content */}
+			<Flex flexDir="column" w="100%">
+				{/* Buttons */}
+				<Flex w="100%" p="2vh">
+					<Button w="100%">
+						<FaPlus />
+						<Text ml="1vh">Request new PEG</Text>
+					</Button>
+					<Button w="100%" ml="1vh">
+						<FaPlus />
+						<Text ml="1vh">Request new Feedback</Text>
+					</Button>
+				</Flex>
+				{/* Dynamic content screen */}
+				<Flex h="100%" justify="center" align="center">
+					<Switch>
+						<Route path="/register">
+							<RegisterScreen />
+						</Route>
+						<Route path="/login">
+							<LoginScreen />
+						</Route>
+						<Route path="/">
+							<HomeScreen />
+						</Route>
+					</Switch>
+				</Flex>
+			</Flex>
+		</Flex>
+	);
 
-      {/* Right content */}
-      <Flex flexDir="column" w="100%">
-        {/* Buttons */}
-        {loggedIn && (
-          <Flex w="100%" p="2vh">
-            <Button w="100%">
-              <FaPlus />
-              <Text ml="1vh">Request new PEG</Text>
-            </Button>
-            <Button w="100%" ml="1vh">
-              <FaPlus />
-              <Text ml="1vh">Request new Feedback</Text>
-            </Button>
-          </Flex>
-        )}
-        {/* Dynamic content screen */}
-        <Flex h="100%" justify="center" align="center">
-          <Switch>
-            <Route path="/register">
-              <RegisterScreen />
-            </Route>
-            <Route path="/login">
-              <LoginScreen />
-            </Route>
-            <Route path="/">
-              <HomeScreen />
-            </Route>
-          </Switch>
-        </Flex>
-      </Flex>
-    </Flex>
-  );
 }
 
 export default App;
