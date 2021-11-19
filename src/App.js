6104ef54c30d993c3398667c1b711001108647d9
Radutiu Horatiu <61684220+Radutiu-Horatiu@users.noreply.github.com>
2021-11-19 14:17:00 +0200

    merged and fixed from auth2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import "./App.css";
 import React from "react";
-import { doc, setDoc } from "@firebase/firestore";
-import { db } from "./firebase";
-import axios from "axios";
 import HomeScreen from "./screens/HomeScreen/HomeScreen";
 import { Route, Switch } from "react-router";
 import LoginScreen from "./screens/Login/LoginScreen";
@@ -12,30 +9,15 @@ import { Flex, Text } from "@chakra-ui/layout";
 import { Button } from "@chakra-ui/button";
 import { FaPlus } from "react-icons/fa";
 function App() {
-	React.useEffect(() => {
-		// firebase connection example
-		// (async () => {
-		// 	await setDoc(doc(db, "test", "test"), {
-		// 		data: "This is a test.",
-		// 	});
-		// })();
-		// fastapi example
-		// (async () => {
-		//   const response = await axios.request({
-		//     method: "GET",
-		//     url: "http://127.0.0.1:8000/ceva",
-		//   });
-		//   console.log(response.data);
-		// })();
-	}, []);
+  React.useEffect(() => {
     // fastapi example
-    (async () => {
-      const response = await axios.request({
-        method: "GET",
-        url: "http://127.0.0.1:8000/ceva",
-      });
-      console.log(response.data);
-    })();
+    // (async () => {
+    //   const response = await axios.request({
+    //     method: "GET",
+    //     url: "http://127.0.0.1:8000/ceva",
+    //   });
+    //   console.log(response.data);
+    // })();
   }, []);
 
   return (
