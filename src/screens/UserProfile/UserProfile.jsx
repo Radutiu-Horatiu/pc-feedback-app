a78c438d24738ccc5ed01c11295a374f40a85801
Radutiu Horatiu <61684220+Radutiu-Horatiu@users.noreply.github.com>
2021-11-26 01:53:01 +0200

    format layout (#9)
    Co-authored-by: AndreiutDev <popaandrei17082000@gmail.com>

--- src/screens/UserProfile/UserProfile.jsx
+++ src/screens/UserProfile/UserProfile.jsx
@@ -1,13 +1,13 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, Grid, Box } from '@chakra-ui/react'
 import { Center, Text } from '@chakra-ui/layout'
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { userActions } from '../../store/user/user-slice';
 import { Tag } from '@chakra-ui/tag';
-import { FormControl, FormLabel, Select, FormErrorMessage, FormHelperText, Input} from "@chakra-ui/react"
+import { FormControl, FormLabel, Select, FormErrorMessage, FormHelperText, Input } from "@chakra-ui/react"
 export default function UserProfile() {
-  
-    
+
+
     const dispatch = useDispatch();
     const username = useSelector(state => state.user.username);
     const email = useSelector(state => state.user.email);
@@ -17,11 +17,6 @@ export default function UserProfile() {
     const personalNumber = useSelector(state => state.user.personalNumber);
     const careerLevel = useSelector(state => state.user.careerLevel);
     const organizationalAssignments = useSelector(state => state.user.organizationalAssignments);
-
-    /**useEffect(() => {
-		dispatch(userActions.setUsername({username: "Arekkusu Poppu"}));
-        
-	}, [dispatch, username]);**/
     return (
     <Flex flexDirection="column" justify="left" align="left">
 
@@ -40,33 +35,38 @@ export default function UserProfile() {
             <Input type="Name" style={{width: "370px"}} />
             <FormHelperText>Enter your first and last Name</FormHelperText >
         </FormControl>
+                <FormControl id="Name" isDisabled={false} padding="3">
+                    <FormLabel>Full Name: </FormLabel>
+                    <Input type="Name" style={{ width: "370px" }} />
+                    <FormHelperText>Enter your first and last Name</FormHelperText >
+                </FormControl>
 
-        <FormControl id="FiscalYear" isDisabled={true} padding="3">
-            <FormLabel>Fiscal Year: </FormLabel>
-            <Input type="FiscalYear" style={{width: "370px"}} />
-        </FormControl>
+                <FormControl id="FiscalYear" isDisabled={true} padding="3">
+                    <FormLabel>Fiscal Year: </FormLabel>
+                    <Input type="FiscalYear" style={{ width: "370px" }} />
+                </FormControl>
 
-        <FormControl id="PersonalNumber" isDisabled={true} padding="3">
-            <FormLabel>Personal Number: </FormLabel>
-            <Input type="PersonalNumber" style={{width: "370px"}} />
-        </FormControl>
+                <FormControl id="PersonalNumber" isDisabled={true} padding="3">
+                    <FormLabel>Personal Number: </FormLabel>
+                    <Input type="PersonalNumber" style={{ width: "370px" }} />
+                </FormControl>
 
-        <FormControl id="CareerLevel" padding="3">
-            <FormLabel>Career Level: </FormLabel>
-                <Select placeholder="Select career level" style={{width: "370px"}} >
-                    <option>Option1</option>
-                    <option>Option2</option>
-                </Select>
-        </FormControl>
+                <FormControl id="CareerLevel" padding="3">
+                    <FormLabel>Career Level: </FormLabel>
+                    <Select placeholder="Select career level" style={{ width: "370px" }} >
+                        <option>Option1</option>
+                        <option>Option2</option>
+                    </Select>
+                </FormControl>
 
-        <FormControl id="OrganizationalAssignments" padding="3">
-            <FormLabel>Organizational Assignments: </FormLabel>
-                <Select placeholder="Select Organizational Assignments" style={{width: "370px"}} >
-                    <option>Option1</option>
-                    <option>Option2</option>
-                </Select>
-        </FormControl>
-
-    </Flex>
+                <FormControl id="OrganizationalAssignments" padding="3">
+                    <FormLabel>Organizational Assignments: </FormLabel>
+                    <Select placeholder="Select Organizational Assignments" style={{ width: "370px" }} >
+                        <option>Option1</option>
+                        <option>Option2</option>
+                    </Select>
+                </FormControl>
+            </Grid>
+        </Flex>
     )
 }
