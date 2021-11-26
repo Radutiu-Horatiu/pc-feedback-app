409888a34b921200acdbec4a647cf782fc7a5c6f
Mihai Oancea <50120497+mihib2000@users.noreply.github.com>
2021-11-26 13:51:14 +0200

    api call and display pegs

--- src/screens/RequestPeg/RequestPeg.jsx
+++ src/screens/RequestPeg/RequestPeg.jsx
@@ -1,40 +1,73 @@
 import { Flex } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption } from "@chakra-ui/react";
 export default function RequestPeg() {
+	const [pegs, setpegs] = useState(null);
+	useEffect(() => {
+		fetch("http://127.0.0.1:8000/allPegs/")
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				setpegs(data);
+			});
+	}, []);
+	const refresh = () => {
+		console.log("refresh");
+	};
 	return (
 		<Flex>
 			<Table variant="simple">
 				<TableCaption>PEG REQUESTS</TableCaption>
 				<Thead>
 					<Tr>
-						<Th>To convert</Th>
-						<Th>into</Th>
-						<Th isNumeric>multiply by</Th>
+						<Th isNumeric>Criteria</Th>
+						<Th>Customer name</Th>
+						<Th>Date of PEG</Th>
+						<Th>Evaluator Name</Th>
+						<Th isNumeric>Fiscal year</Th>
+						<Th>Manager name</Th>
+						<Th isNumeric>No. of days evaluated</Th>
+						<Th isNumeric>Personal Number</Th>
+						<Th>Project id</Th>
+						<Th>Project name</Th>
+						<Th>Status</Th>
+						<Th>User id</Th>
 					</Tr>
 				</Thead>
 				<Tbody>
-					<Tr>
-						<Td>inches</Td>
-						<Td>millimetres (mm)</Td>
-						<Td isNumeric>25.4</Td>
-					</Tr>
-					<Tr>
-						<Td>feet</Td>
-						<Td>centimetres (cm)</Td>
-						<Td isNumeric>30.48</Td>
-					</Tr>
-					<Tr>
-						<Td>yards</Td>
-						<Td>metres (m)</Td>
-						<Td isNumeric>0.91444</Td>
-					</Tr>
+					{pegs?.map((peg) => {
+						return (
+							<Tr>
+								<Td isNumeric>{peg["Criteria"]}</Td>
+								<Td>{peg["Customer name"]}</Td>
+								<Td>{new Date(peg["Date of PEG"]).toLocaleDateString("en-US")}</Td>
+								<Td>{peg["Evaluator name"]}</Td>
+								<Td>{peg["Fiscal year"]}</Td>
+								<Td>{peg["Manager name"]}</Td>
+								<Td>{peg["Number of project days evaluated"]}</Td>
+								<Td>{peg["Personal Number"]}</Td>
+								<Td>{peg["Project id"]}</Td>
+								<Td>{peg["Project name"]}</Td>
+								<Td>{peg["Status"]}</Td>
+								<Td>{peg["User id"]}</Td>
+							</Tr>
+						);
+					})}
 				</Tbody>
 				<Tfoot>
 					<Tr>
-						<Th>To convert</Th>
-						<Th>into</Th>
-						<Th isNumeric>multiply by</Th>
+						<Th isNumeric>Criteria</Th>
+						<Th>Customer name</Th>
+						<Th>Date of PEG</Th>
+						<Th>Evaluator Name</Th>
+						<Th isNumeric>Fiscal year</Th>
+						<Th>Manager name</Th>
+						<Th isNumeric>No. of days evaluated</Th>
+						<Th isNumeric>Personal Number</Th>
+						<Th>Project id</Th>
+						<Th>Project name</Th>
+						<Th>Status</Th>
+						<Th>User id</Th>
 					</Tr>
 				</Tfoot>
 			</Table>
