f0932c3de6196845015f9ce2768ecfd161cddda2
AndreiutDev <popaandrei17082000@gmail.com>
2021-12-09 20:37:51 +0200

    POST"

--- src/screens/UserProfile/UserProfile.jsx
+++ src/screens/UserProfile/UserProfile.jsx
@@ -1,9 +1,13 @@
 import { Flex, Grid, Box } from "@chakra-ui/react";
 import { Center, Text } from "@chakra-ui/layout";
 import React, { useEffect } from "react";
+import { Button } from "@chakra-ui/button";
 import { useDispatch, useSelector } from "react-redux";
 import { userActions } from "../../store/user/user-slice";
 import { Tag } from "@chakra-ui/tag";
+import { API } from "../../utils/API";
+import axios from "axios";
+import { useState } from "react";
 import {
   FormControl,
   FormLabel,
@@ -14,7 +18,7 @@ import {
 } from "@chakra-ui/react";
 export default function UserProfile() {
   const dispatch = useDispatch();
-  const username = useSelector((state) => state.user.username);
+  /*const username = useSelector((state) => state.user.username);
   const email = useSelector((state) => state.user.email);
   const name = useSelector((state) => state.user.name);
   const role = useSelector((state) => state.user.role);
@@ -23,59 +27,117 @@ export default function UserProfile() {
   const careerLevel = useSelector((state) => state.user.careerLevel);
   const organizationalAssignments = useSelector(
     (state) => state.user.organizationalAssignments
-  );
+  );*/
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [role, setRole] = useState("");
+  const [fiscalYear, setFiscalYear] = useState(0);
+  const [personalNumber, setPersonalNumber] = useState(0);
+  const [organizationalAssignment, setOrganizationalAssignment] = useState("");
+  const [careerLevel, setCareerLevel] = useState("");
+  const user = useSelector((state) => state.user);
+
+
+  React.useEffect(() => {
+      setCareerLevel(user?.careerLevel);
+      setEmail(user.email);
+      setUsername(user.username);
+      setName(user.name);
+      setFiscalYear(user.fiscalYear);
+      setPersonalNumber(user.personalNumber);
+      setRole(user.role);
+      setOrganizationalAssignment(user.organizationalAssignment);
+  }, []);
+  const editUser = async () => {
+      const myUser = 
+      {
+          uid: user.uid,
+          email: email,
+          username: username,
+          name: name,
+          role: role,
+          fiscalYear: fiscalYear,
+          organizationalAssignment: organizationalAssignment,
+          careerLevel: careerLevel,
+          personalNumber: personalNumber,
+      }
+      await axios.request({
+        method: "POST",
+        url: API.backend + "/updateUser?id=NG7QNDVfDOgAPoJrkAovVwLTGWm2",
+        data: myUser,
+      });
+  }
   return (
     <Flex flexDirection="column" justify="left" align="left">
-      <FormControl id="Email" isDisabled={true} padding="3">
+        <Grid templateColumns='repeat(2, 1fr)' gap={6}>
+      <FormControl id="Email" isReadOnly={true} padding="3">
         <FormLabel>Email: </FormLabel>
-        <Input type="Email" style={{ width: "370px" }} />
+        <Input value={email} onChange={(e) => {setEmail(e.target.value)}} type="Email" style={{ width: "370px" }} />
       </FormControl>
 
-      <FormControl id="Username" isDisabled={true} padding="3">
+      <FormControl id="Username" padding="3">
         <FormLabel>Username: </FormLabel>
-        <Input type="Username" style={{ width: "370px" }} />
+        <Input value={username} onChange={(e) => {setUsername(e.target.value)}} type="Username" style={{ width: "370px" }} />
       </FormControl>
 
-      <FormControl id="Name" isDisabled={false} padding="3">
+      <FormControl id="Name"  padding="3">
         <FormLabel>Full Name: </FormLabel>
-        <Input type="Name" style={{ width: "370px" }} />
+        <Input value={name} onChange={(e) => {setName(e.target.value)}} type="Name" style={{ width: "370px" }} />
         <FormHelperText>Enter your first and last Name</FormHelperText>
       </FormControl>
-      <Grid>
-        <FormControl id="Name" isDisabled={false} padding="3">
-          <FormLabel>Full Name: </FormLabel>
-          <Input type="Name" style={{ width: "370px" }} />
-          <FormHelperText>Enter your first and last Name</FormHelperText>
-        </FormControl>
 
-        <FormControl id="FiscalYear" isDisabled={true} padding="3">
+        <FormControl id="FiscalYear" isReadOnly={true} padding="3">
           <FormLabel>Fiscal Year: </FormLabel>
           <Input type="FiscalYear" style={{ width: "370px" }} />
         </FormControl>
 
-        <FormControl id="PersonalNumber" isDisabled={true} padding="3">
+        <FormControl id="PersonalNumber" isReadOnly={true} padding="3">
           <FormLabel>Personal Number: </FormLabel>
           <Input type="PersonalNumber" style={{ width: "370px" }} />
         </FormControl>
 
         <FormControl id="CareerLevel" padding="3">
           <FormLabel>Career Level: </FormLabel>
-          <Select placeholder="Select career level" style={{ width: "370px" }}>
+          <Select
+                placeholder="Select career level" style={{ width: "370px" }}
+                value={careerLevel}
+                onChange={(e) => {setCareerLevel(e.target.value)}}
+           >
             <option>Option1</option>
             <option>Option2</option>
           </Select>
         </FormControl>
-
         <FormControl id="OrganizationalAssignments" padding="3">
           <FormLabel>Organizational Assignments: </FormLabel>
           <Select
             placeholder="Select Organizational Assignments"
             style={{ width: "370px" }}
+            value={organizationalAssignment}
+            onChange={(e) => {setOrganizationalAssignment(e.target.value)}}
           >
             <option>Option1</option>
             <option>Option2</option>
           </Select>
         </FormControl>
+        <FormControl id="Role" padding="3">
+          <FormLabel>Roles: </FormLabel>
+          <Select
+            placeholder="Select Organizational Assignments"
+            style={{ width: "370px" }}
+            value={role}
+            onChange={(e) => {setRole(e.target.value)}}
+          >
+            <option>Option1</option>
+            <option>Option2</option>
+          </Select>
+        </FormControl>
+        <Button
+            style={{ marginTop: "43px", width: "370px", marginLeft: "15px" }}
+            colorScheme="teal"
+            onClick={() => {editUser();}}
+        >
+            Edit User</Button>
       </Grid>
     </Flex>
   );
