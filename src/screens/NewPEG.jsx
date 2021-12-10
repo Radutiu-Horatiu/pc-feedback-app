e47a2ed4b92c96d6f78f4952c321f207a96ce5b4
Radutiu Horatiu <61684220+Radutiu-Horatiu@users.noreply.github.com>
2021-12-10 15:15:04 +0200

    New peg request (#13)
    * new peg request
    
    * fixed bug with backend
    
    Co-authored-by: MariaP78 <maria_minodora07@yahoo.com>

--- src/screens/NewPEG.jsx
+++ src/screens/NewPEG.jsx
@@ -1,68 +1,135 @@
-import { Flex } from "@chakra-ui/react";
-import { Heading, Text } from "@chakra-ui/layout";
-import React, { useEffect } from "react";
+import { Flex, Button } from "@chakra-ui/react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { userActions } from "../store/user/user-slice";
-import {
-    FormControl,
-    FormLabel,
-    FormErrorMessage,
-    Input,
-    FormHelperText,
-} from "@chakra-ui/react"
-import { auth } from "../firebase";
+import { FormControl, FormLabel, Input, Box } from "@chakra-ui/react";
+import axios from "axios";
 
 export default function NewPEG() {
-    const dispatch = useDispatch();
-    const user = useSelector((state) => state.user);
+  const user = useSelector((state) => state.user);
+  const [pegId, setpegId] = useState();
+  const [employeeName, setemployeeName] = useState();
+  const [currentCareerLevel, setcurrentCareerLevel] = useState();
+  const [organizationalAssignment, setorganizationalAssignment] = useState();
+  const [pegDate, setpegDate] = useState();
+  const [projectId, setprojectId] = useState();
+  const [customerName, setcustomerName] = useState();
+  const [nameOfProject, setnameOfProject] = useState();
+  const [nameOfProjectManager, setnameOfProjectManager] = useState();
+  const [nameOfProjectEvaluator, setnameOfProjectEvaluator] = useState();
+  const [numberProjectDaysEvaluated, setnumberProjectDaysEvaluated] =
+    useState();
 
-    return (
-        <Flex flexDir="column" w="60vh">
-            <Heading>Request new PEG</Heading>
-            <FormControl id="employee-name" isDisabled>
-                <FormLabel>Employee name</FormLabel>
-                <Input type="text" />
-            </FormControl>
-            <FormControl id="personnel-number">
-                <FormLabel>Personnel number</FormLabel>
-                <Input type="text" />
-            </FormControl>
-            <FormControl id="current-career-level">
-                <FormLabel>Current career level</FormLabel>
-                <Input type="text" />
-            </FormControl>
-            <FormControl id="organizational-assignment">
-                <FormLabel>Organizational assignment</FormLabel>
-                <Input type="" />
-            </FormControl>
-            <FormControl id="peg-date">
-                <FormLabel>Date of PEG</FormLabel>
-                <Input type="date" />
-            </FormControl>
-            <FormControl id="project-id">
-                <FormLabel>Project ID</FormLabel>
-                <Input type="text" />
-            </FormControl>
-            <FormControl id="customer-name">
-                <FormLabel>Customer name</FormLabel>
-                <Input type="text" />
-            </FormControl>
-            <FormControl id="name-of-project">
-                <FormLabel>Name of the project</FormLabel>
-                <Input type="text" />
-            </FormControl>
-            <FormControl id="name-of-project-manager">
-                <FormLabel>Name of the project</FormLabel>
-                <Input type="text" />
-            </FormControl>
-            <FormControl id="name-of-evaluator">
-                <FormLabel>Name of the project</FormLabel>
-                <Input type="text" />
-            </FormControl>
-            <FormControl id="number-of-project-days-evaluated">
-                <FormLabel>Number of project days evaluated</FormLabel>
-                <Input type="text" />
-            </FormControl>
-        </Flex>
-    );
+  const clickSubmitRequestButton = async () => {
+    let peg = {
+      fiscal_year: 2021,
+      employee_name: employeeName,
+      peg_id: pegId,
+      current_career_level: currentCareerLevel,
+      org_assignment: organizationalAssignment,
+      date_of_peg: new Date(pegDate).toLocaleString("en-us"),
+      project_id: projectId,
+      customer_name: customerName,
+      name_of_project: nameOfProject,
+      name_of_manager: nameOfProjectManager,
+      evaluator_name: nameOfProjectEvaluator,
+      no_of_project_days_evaluated: 1,
+      user_id: "test",
+      criteria: 1,
+      status: "pending"
+    };
+
+    await axios.request({
+      method: "POST",
+      url: "http://127.0.0.1:8000/addPeg/",
+      data: peg,
+    });
+  };
+
+  return (
+    <Flex flexDir="column" w="60vh">
+      <FormControl id="employee-name" isDisabled>
+        <FormLabel>Employee name</FormLabel>
+        <Input
+          type="text"
+          onChange={(event) => setemployeeName(event.target.value)}
+        />
+      </FormControl>
+      <FormControl id="personnel-number">
+        <FormLabel>Personnel number</FormLabel>
+        <Input type="text" onChange={(event) => setpegId(event.target.value)} />
+      </FormControl>
+      <FormControl id="current-career-level">
+        <FormLabel>Current career level</FormLabel>
+        <Input
+          type="text"
+          onChange={(event) => setcurrentCareerLevel(event.target.value)}
+        />
+      </FormControl>
+      <FormControl id="organizational-assignment">
+        <FormLabel>Organizational assignment</FormLabel>
+        <Input
+          type=""
+          onChange={(event) => setorganizationalAssignment(event.target.value)}
+        />
+      </FormControl>
+      <FormControl id="peg-date">
+        <FormLabel>Date of PEG</FormLabel>
+        <Input
+          type="date"
+          onChange={(event) => setpegDate(event.target.value)}
+        />
+      </FormControl>
+      <FormControl id="project-id">
+        <FormLabel>Project ID</FormLabel>
+        <Input
+          type="text"
+          onChange={(event) => setprojectId(event.target.value)}
+        />
+      </FormControl>
+      <FormControl id="customer-name">
+        <FormLabel>Customer name</FormLabel>
+        <Input
+          type="text"
+          onChange={(event) => setcustomerName(event.target.value)}
+        />
+      </FormControl>
+      <FormControl id="name-of-project">
+        <FormLabel>Name of the project</FormLabel>
+        <Input
+          type="text"
+          onChange={(event) => setnameOfProject(event.target.value)}
+        />
+      </FormControl>
+      <FormControl id="name-of-project-manager">
+        <FormLabel>Name of the project manager</FormLabel>
+        <Input
+          type="text"
+          onChange={(event) => setnameOfProjectManager(event.target.value)}
+        />
+      </FormControl>
+      <FormControl id="name-of-evaluator">
+        <FormLabel>Name of the project evaluator</FormLabel>
+        <Input
+          type="text"
+          onChange={(event) => setnameOfProjectEvaluator(event.target.value)}
+        />
+      </FormControl>
+      <FormControl id="number-of-project-days-evaluated">
+        <FormLabel>Number of project days evaluated</FormLabel>
+        <Input
+          type="text"
+          onChange={(event) =>
+            setnumberProjectDaysEvaluated(event.target.value)
+          }
+        />
+      </FormControl>
+      <Box align="center" padding="5">
+        <FormControl>
+          <Button colorScheme="teal" onClick={clickSubmitRequestButton}>
+            Submit request
+          </Button>
+        </FormControl>
+      </Box>
+    </Flex>
+  );
 }
